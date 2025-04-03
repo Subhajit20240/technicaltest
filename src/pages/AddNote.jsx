e0d857175a660ad9468f5e2fa04cdedcaed230a6
Subhajit20240 <subhajit4582@gmail.com>
2025-04-03 15:58:56 +0530

    techni

--- src/pages/AddNote.jsx
+++ src/pages/AddNote.jsx
@@ -17,14 +17,14 @@ const AddNote = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="bg-white shadow-md rounded-lg p-6 w-[600px] h-[400px] flex flex-col justify-center">
+      <div className="bg-gray-300 shadow-md rounded-lg p-6 w-[600px] h-[400px] flex flex-col justify-center">
         <h2 className="text-2xl font-bold text-center mb-4">Add Your Note Here</h2>
         <form onSubmit={handleSubmit} className="flex flex-col">
           <input
-            type="text" placeholder="Title" value={title}  onChange={(e) => setTitle(e.target.value)}  className="border border-gray-300 p-2 rounded mb-2"
+            type="text" placeholder="Title" value={title}  onChange={(e) => setTitle(e.target.value)}  className="border border-gray-300 p-2 rounded mb-2 text-black"
           />
           <textarea
-            placeholder="Content"  value={content}  onChange={(e) => setContent(e.target.value)}  className="border border-gray-300 p-2 rounded mb-2"
+            placeholder="Content"  value={content}  onChange={(e) => setContent(e.target.value)}  className="border border-gray-300 p-2 rounded mb-2 text-black"
           />
           <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-lg">
             Save Note
